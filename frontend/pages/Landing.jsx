b214b27b71d6722cb9ed008d146fd5b478816384
Mathew Hultquist <mathew@gates.services>
2023-01-07 12:27:17 -0500

    Temp card component for testing

--- frontend/pages/Landing.jsx
+++ frontend/pages/Landing.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Card from '../components/CardDisplayComponent.jsx';
 
-const Landing = () => <h1>Hello World</h1>;
+const Landing = () => (
+  <div className='LandingPage'>
+    <Card cardId='test' />
+  </div>
+);
 
 export default Landing;
