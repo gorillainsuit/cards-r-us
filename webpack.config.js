bef2d343b9e7e5cd56d1ec55c36a1906021edc09
roy bae <roybae@roys-Laptop.home>
2023-01-07 15:53:52 -0800

    Feature - Finished with the 404 error page

--- webpack.config.js
+++ webpack.config.js
@@ -34,14 +34,6 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
     ],
   },
 
