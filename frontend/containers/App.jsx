b70f78fdaacf3c3eadd43fd3335f0158c164e736
Mathew Hultquist <mathew@gates.services>
2023-01-09 13:06:38 -0500

    Began adding responsiveness

--- frontend/containers/App.jsx
+++ frontend/containers/App.jsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Outlet, useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import Card from '../components/CardDisplayComponent';
+import Avatar from '@mui/joy/Avatar';
+import Button from '@mui/joy/Button';
+import IconButton from '@mui/joy/IconButton';
+import FilterList from '@mui/icons-material/FilterList';
+import Divider from '@mui/joy/Divider';
+import CircularProgress from '@mui/joy/CircularProgress';
+import ChevronRight from '@mui/icons-material/ChevronRight';
+import ChevronLeft from '@mui/icons-material/ChevronLeft';
 
 import MobileExpandIcon from '../images/icons/list.svg';
 // TODO: add preview image based on S3 url instead of placeholder
@@ -12,94 +20,309 @@ let testData = [
   {
     id: '1',
     image: Placeholder,
-    prompt: 'Gorilla in suit',
+    prompt: 'Gorilla in suit1',
+    authored: true,
   },
   {
     id: '2',
     image: Placeholder,
-    prompt: 'Gorilla in suit',
+    prompt: 'Gorilla in suit2',
+    authored: false,
   },
   {
     id: '3',
     image: Placeholder,
-    prompt: 'Gorilla in suit',
+    prompt: 'Gorilla in suit3',
+    authored: true,
   },
   {
     id: '4',
     image: Placeholder,
-    prompt: 'Gorilla in suit',
+    prompt: 'Gorilla in suit4',
+    authored: false,
+  },
+  {
+    id: '1',
+    image: Placeholder,
+    prompt: 'Gorilla in suit1',
+    authored: true,
+  },
+  {
+    id: '2',
+    image: Placeholder,
+    prompt: 'Gorilla in suit2',
+    authored: false,
+  },
+  {
+    id: '3',
+    image: Placeholder,
+    prompt: 'Gorilla in suit3',
+    authored: true,
+  },
+  {
+    id: '4',
+    image: Placeholder,
+    prompt: 'Gorilla in suit4',
+    authored: false,
+  },
+  {
+    id: '1',
+    image: Placeholder,
+    prompt: 'Gorilla in suit1',
+    authored: true,
+  },
+  {
+    id: '2',
+    image: Placeholder,
+    prompt: 'Gorilla in suit2',
+    authored: false,
+  },
+  {
+    id: '3',
+    image: Placeholder,
+    prompt: 'Gorilla in suit3',
+    authored: true,
+  },
+  {
+    id: '4',
+    image: Placeholder,
+    prompt: 'Gorilla in suit4',
+    authored: false,
+  },
+  {
+    id: '1',
+    image: Placeholder,
+    prompt: 'Gorilla in suit1',
+    authored: true,
+  },
+  {
+    id: '2',
+    image: Placeholder,
+    prompt: 'Gorilla in suit2',
+    authored: false,
+  },
+  {
+    id: '3',
+    image: Placeholder,
+    prompt: 'Gorilla in suit3',
+    authored: true,
+  },
+  {
+    id: '4',
+    image: Placeholder,
+    prompt: 'Gorilla in suit4',
+    authored: false,
+  },
+  {
+    id: '1',
+    image: Placeholder,
+    prompt: 'Gorilla in suit1',
+    authored: true,
+  },
+  {
+    id: '2',
+    image: Placeholder,
+    prompt: 'Gorilla in suit2',
+    authored: false,
+  },
+  {
+    id: '3',
+    image: Placeholder,
+    prompt: 'Gorilla in suit3',
+    authored: true,
+  },
+  {
+    id: '4',
+    image: Placeholder,
+    prompt: 'Gorilla in suit4',
+    authored: false,
+  },
+  {
+    id: '1',
+    image: Placeholder,
+    prompt: 'Gorilla in suit1',
+    authored: true,
+  },
+  {
+    id: '2',
+    image: Placeholder,
+    prompt: 'Gorilla in suit2',
+    authored: false,
+  },
+  {
+    id: '3',
+    image: Placeholder,
+    prompt: 'Gorilla in suit3',
+    authored: true,
+  },
+  {
+    id: '4',
+    image: Placeholder,
+    prompt: 'Gorilla in suit4',
+    authored: false,
+  },
+  {
+    id: '1',
+    image: Placeholder,
+    prompt: 'Gorilla in suit1',
+    authored: true,
+  },
+  {
+    id: '2',
+    image: Placeholder,
+    prompt: 'Gorilla in suit2',
+    authored: false,
+  },
+  {
+    id: '3',
+    image: Placeholder,
+    prompt: 'Gorilla in suit3',
+    authored: true,
+  },
+  {
+    id: '4',
+    image: Placeholder,
+    prompt: 'Gorilla in suit4',
+    authored: false,
+  },
+  {
+    id: '1',
+    image: Placeholder,
+    prompt: 'Gorilla in suit1',
+    authored: true,
+  },
+  {
+    id: '2',
+    image: Placeholder,
+    prompt: 'Gorilla in suit2',
+    authored: false,
+  },
+  {
+    id: '3',
+    image: Placeholder,
+    prompt: 'Gorilla in suit3',
+    authored: true,
+  },
+  {
+    id: '4',
+    image: Placeholder,
+    prompt: 'Gorilla in suit4',
+    authored: false,
+  },
+  {
+    id: '1',
+    image: Placeholder,
+    prompt: 'Gorilla in suit1',
+    authored: true,
+  },
+  {
+    id: '2',
+    image: Placeholder,
+    prompt: 'Gorilla in suit2',
+    authored: false,
+  },
+  {
+    id: '3',
+    image: Placeholder,
+    prompt: 'Gorilla in suit3',
+    authored: true,
+  },
+  {
+    id: '4',
+    image: Placeholder,
+    prompt: 'Gorilla in suit4',
+    authored: false,
   },
 ];
 
 const App = ({ children }) => {
-  const [displaySideBar, setDispalySideBar] = useState(true);
-  const [cards, setCards] = useState([]);
+  const [displaySideBar, setDisplaySideBar] = useState(true);
+  const [filterCardsByAuthor, setFilterCardsByAuthor] = useState(false);
+  const [cards, setCards] = useState(null);
   const location = useLocation();
 
   useEffect(() => {
     // TODO: Replace this with a fetch to backend
-    setTimeout(() => setCards(testData), 600);
+    // TODO: fix bug where "fetch" occurs on every filter
+
+    setTimeout(
+      () =>
+        setCards(
+          testData.filter((card) =>
+            filterCardsByAuthor ? card.authored : true
+          )
+        ),
+      600
+    );
   });
 
   const handleCardDelete = (e, id) => {
     e.preventDefault();
     // TODO: have delete card in database as well
     testData = testData.filter((card) => card.id !== id);
-    setCards(cards.filter((card) => card.id !== id));
+    setCards(testData);
   };
+
+  // TODO: Refactor the jank responsiveness
 
   return (
     <div className='MainContent'>
-      <div className='SideBar'>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/cards'>Gallary</Link>
-            </li>
-          </ul>
-          <div className='PopDown'>
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                setDispalySideBar(!displaySideBar);
-              }}>
-              <MobileExpandIcon />
-            </a>
-            <div
-              className='PopDownContent'
-              style={{ display: !displaySideBar ? 'flex' : 'none' }}>
-              <ul>
-                <li>
-                  <Link to='/'>Home</Link>
-                </li>
-                <li>
-                  <Link to='/cards'>Gallary</Link>
-                </li>
-              </ul>
-            </div>
+      <div className='SideBar' style={{ width: displaySideBar ? '' : '5em' }}>
+        <div className='User'>
+          <Avatar
+            style={{ display: displaySideBar ? '' : 'none' }}
+            alt='Placeholder'
+            src={Placeholder}
+          />
+          <h2 style={{ display: displaySideBar ? '' : 'none' }}>Placeholder</h2>
+          <IconButton
+            variant='plain'
+            onClick={() => setDisplaySideBar(!displaySideBar)}>
+            {displaySideBar ? <ChevronLeft /> : <ChevronRight />}
+          </IconButton>
+        </div>
+        <div
+          className='SideBarContent'
+          style={{ display: displaySideBar ? '' : 'none' }}>
+          <Divider orientation='horizontal' />
+          <div className='MainControls'>
+            <ul>
+              <li>
+                <Button
+                  onClick={() => setFilterCardsByAuthor(!filterCardsByAuthor)}
+                  startDecorator={<FilterList />}
+                  variant='soft'>
+                  {filterCardsByAuthor ? 'Show All' : 'Show Sent'}
+                </Button>
+              </li>
+            </ul>
           </div>
-        </nav>
+          <div className='SecondaryControls'>
+            <Button onClick={() => {}} variant='soft'>
+              Logout
+            </Button>
+          </div>
+        </div>
       </div>
       <div className='Content'>
         {location === '/customize' ? (
           <Outlet />
         ) : (
           <div className='Gallary'>
-            {cards.length ? (
-              cards.map((card) => (
+            {(cards ?? false) && cards.length ? (
+              cards.map((card, i) => (
                 <Card
-                  key={card.id}
+                  key={i}
                   cardId={card.id}
                   image={card.image}
                   prompt={card.prompt}
                   deleteFunction={handleCardDelete}
                 />
               ))
+            ) : cards === null ? (
+              <CircularProgress color='primary' value={25} variant='soft' />
             ) : (
-              <h1>No Cards...</h1>
+              <h1>No Cards.</h1>
             )}
           </div>
         )}
