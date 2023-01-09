1f8e171b25777d9fd394377363b60c20b9e34fa2
JamesJunJieHuang <JamesjunjieHuang@gmail.com>
2023-01-09 18:36:06 -0500

    fixed landing page route in server.js, added signup route, fixed up login route

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,17 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// '/login' Endpoint 
-
-//GET when login page initially loads
-//respond with html file
-router.get('/', (req, res) => {
-  res.send('.../frontend/index.html');
-});
 
 //POST when user tries to log in
-router.post('/', authController.getUser, authController.signUp, (req, res) => {
-  res.status(200).json(req.user);
+router.post('/', authController.getUser, (req, res) => {
+  res.status(200).json(res.locals.userData);
 });
 
 module.exports = router;
