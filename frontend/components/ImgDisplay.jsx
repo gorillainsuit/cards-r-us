4a27b4875bebee1e54516fcbd2e8b2541c9a96e8
zhiyi15 <zhiyihuang1994@gmail.com>
2023-01-10 10:39:40 -0800

    added ImgDisplay file & added more details in createCard

---
+++ frontend/components/ImgDisplay.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+ 
+const ImgDisplay = (props) => {
+  
+  const { imgList } = props;
+  const {selectedImage, setSelectedImage} = props;
+
+  const handleClick = (e) => {
+    setSelectedImage(e.key)
+  }
+
+  return ( 
+
+    <div className="img-result">
+      {imgList.data.map((el.url, i) => 
+        <div>   
+          <img 
+          src={el.url} 
+          key={i}
+          />
+        </div>
+      )}
+    </div>
+   );
+}
+ 
+export default ImgDisplay;
