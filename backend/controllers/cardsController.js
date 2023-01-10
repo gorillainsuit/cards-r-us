abe2c31f6119c097cd2418c9d20c3a4b76ff143d
JamesJunJieHuang <JamesjunjieHuang@gmail.com>
2023-01-09 20:10:50 -0500

    added comments

--- backend/controllers/cardsController.js
+++ backend/controllers/cardsController.js
@@ -13,6 +13,7 @@ cardsController.getCards = async (req, res, next) => {
 cardsController.deleteCard = async (req, res, next) => {
   //delete card specific to a user from DB
   await User.findOneAndDelete({ username, cardID });
+  //delete according to index, update according to id or index
   //retrieve updated cards collection data from DB
   const cards = await User.findOne({ username });
   //pass updated cards collection data into res.locals
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,6 +4,9 @@ const authController = require('../controllers/authController');
 
 
 //POST when user tries to log in
+
+//hash password before it's saved to database
+
 router.post('/', authController.getUser, (req, res) => {
   res.status(200).json(res.locals.userData);
 });
