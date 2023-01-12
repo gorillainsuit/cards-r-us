e72ed5c20f510d4c0105efa3fff5b4c1e6074848
JamesJunJieHuang <JamesjunjieHuang@gmail.com>
2023-01-11 20:50:02 -0500

    login and signup route PR

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -26,9 +26,7 @@ const userSchema = new Schema({
 userSchema.pre('save', function (next) {
   bcrypt.hash(this.password, SALT_WORK_FACTOR, (err, hash) => {
     // Store hash in your password DB.
-    console.log('this.password: ', this.password);
     this.password = hash;
-    console.log('hash: ', hash);
     next();
   });
 });
