6b373df843addbcca017282cbd386301dda66df6
Mathew Hultquist <mathew@gates.services>
2023-01-11 20:46:16 -0500

    Added userId to sessions

--- backend/models/sessionsModel.js
+++ backend/models/sessionsModel.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const sessionSchema = new Schema({
   cookieId: { type: String, required: true, unique: true },
-  createdAt: { type: Date, expires: 30, default: Date.now }
+  userId: { type: String, required: true, unique: true },
+  createdAt: { type: Date, expires: 30, default: Date.now },
 });
 
 module.exports = mongoose.model('Session', sessionSchema);
