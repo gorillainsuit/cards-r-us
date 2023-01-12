a26de0179b3dddf84bcf5e0880bc6a1f9bb737e2
Mathew Hultquist <mathew@gates.services>
2023-01-12 18:44:48 -0500

    Began adding oauth route

---
+++ backend/routes/oauth.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { ModuleFilenameHelpers } = require('webpack');
+const router = express.Router();
+const sessionController = require('../controllers/sessionController');
+
+module.exports = router;
