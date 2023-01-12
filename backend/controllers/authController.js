7f8ed488b24b97991033635ca27f0aa99ee4c390
JamesJunJieHuang <JamesjunjieHuang@gmail.com>
2023-01-11 20:50:02 -0500

    login and signup route PR

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,36 +2,32 @@ const User = require('../models/UserModel.js');
 const bcrypt = require('bcrypt');
 
 const authController = {
-  async getUser(req, res, next) {
-    try {
-      const { username, password } = req.body[0];
-      const userData = await User.findOne({ username, password });
-      //   res.locals.userData = userData;
-      if (userData === null) {
-        console.log('user not found');
-        return;
-      }
-      console.log('userData: ', userData);
-      console.log(`User '${username}' found`);
-      return next();
-    } catch (e) {
-      return next({
-        log: 'Middleware error caught in authController - getUser failed',
-        status: 500,
-        message: { err: e.message },
-      });
-    }
-  },
+//   async getUser(req, res, next) {
+//     try {
+//       const { username, password } = req.body;
+//       const userData = await User.findOne({ username, password });
+//       if (userData === null) {
+//         console.log('user not found');
+//         return;
+//       }
+//       console.log('userData: ', userData);
+//       console.log(`User '${username}' found`);
+//       return next();
+//     } catch (e) {
+//       return next({
+//         log: 'Middleware error caught in authController - getUser failed',
+//         status: 500,
+//         message: { err: e.message },
+//       });
+//     }
+//   },
 
   async signUp(req, res, next) {
     try {
-      const { username, password } = req.body[0];
-      const newUser = User.create({ username, password });
-      console.log('reqbody: ', req.body);
-      console.log('username: ', username);
-      console.log('password: ', password);
-      console.log(`User '${newUser}' created`);
-      res.locals.newUser = newUser;
+      const { username, password } = req.body;
+      const newUser = await User.create({ username, password });
+      const {gallery, _id} = newUser;
+      res.locals.newUser = {  username, id:_id, gallery }; 
       return next();
     } catch (e) {
       return next({
@@ -41,13 +37,13 @@ const authController = {
       });
     }
   },
-
+ 
   async verifyUser(req, res, next) {
-    const { username, password } = req.body[0];
+    const { username, password } = req.body;
     let hashedPassword;
     try {
       if (!password) throw new Error('empty password');
-      await User.findOne({ username }, (err, userAccount) => {
+      User.findOne({ username }, (err, userAccount) => {
         if (err || !userAccount) {
           res.redirect('/signup');
         } else {
@@ -62,15 +58,13 @@ const authController = {
                 gallery: userAccount.gallery,
               };
               res.locals.user = user;
-              console.log(user);
               return next();
             } else {
-              console.log('wrong password');
-              res.send('wrong password');
+              res.status(401).json({Err: 'wrong password'});
             }
           });
         }
-      }).clone();
+      });
     } catch (err) {
       return next({
         log: 'Error verifying user',
