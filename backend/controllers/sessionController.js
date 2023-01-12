625cb6cd80c280cedc88e3a2f8f0dc34190eaeb2
Mathew Hultquist <mathew@gates.services>
2023-01-11 20:51:55 -0500

    Added user detail forwarding

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -1,4 +1,5 @@
 const Session = require('../models/sessionModel');
+const User = require('../models/UserModel');
 
 const sessionController = {};
 
@@ -7,33 +8,34 @@ const sessionController = {};
  * verify whether or not the session is still valid.
  */
 sessionController.isLoggedIn = (req, res, next) => {
-	// write code here
-	const ssid = req.cookies['ssid'];
-	if (!ssid) {
-		console.log('No SSID cookie, redirecting...');
-		return res.redirect('/signup');
-	}
-	Session.find({ cookieId: ssid }, (err, records) => {
-		if (!records.length) {
-			console.log('No session in database, redirecting...');
-			return res.redirect('/signup');
-		}
+  // write code here
+  const ssid = req.cookies['ssid'];
+  if (!ssid) {
+    console.log('No SSID cookie, redirecting...');
+    return res.redirect('/signup');
+  }
+  Session.finOne({ cookieId: ssid }, async (err, records) => {
+    if (!records.length) {
+      console.log('No session in database, redirecting...');
+      return res.redirect('/signup');
+    }
 
-		return next();
-	});
+    res.locals.user = await User.findOne({ _id: records.userId });
+    return next();
+  });
 };
 
 /**
  * startSession - create and save a new Session into the database.
  */
 sessionController.startSession = (req, res, next) => {
-	//write code here
-	const ssid = res.locals.user.id;
-	console.log('Creating session for ssid: ', ssid);
-	Session.create({ cookieId: ssid }, (err, document) => {
-		if (err) return next(err);
-		return next();
-	});
+  //write code here
+  const ssid = res.locals.user.id;
+  console.log('Creating session for ssid: ', ssid);
+  Session.create({ cookieId: ssid }, (err, document) => {
+    if (err) return next(err);
+    return next();
+  });
 };
 
 module.exports = sessionController;
