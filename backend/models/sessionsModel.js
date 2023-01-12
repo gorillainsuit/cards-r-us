50ebeba90a4117055315dd8cc385b4b4cfb81b0e
Mathew Hultquist <mathew@gates.services>
2023-01-11 23:37:10 -0500

    Renamed cookieId to userId

--- backend/models/sessionsModel.js
+++ backend/models/sessionsModel.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const sessionSchema = new Schema({
-  cookieId: { type: String, required: true, unique: true },
   userId: { type: String, required: true, unique: true },
   createdAt: { type: Date, expires: 30, default: Date.now },
 });
