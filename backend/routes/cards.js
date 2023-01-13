43608cd26dd09da373544e3ba20516286537d7e7
Michael Ruiz <maruiz.sc1@gmail.com>
2023-01-12 17:09:39 -0800

    adding cards route

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const cardsController = require('../controllers/cardsController.js');
-const sessionController = require('../controllers/sessionController');
+const cardsController = require('../controllers/cardController.js');
+const sessionController = require('../controllers/sessionController.js');
+// this logged using localhost:3000/cards
+// router.get('/', (req, res) => {
+//   console.log('cards router connected');
+// });
 
 //GET REQUEST
 router.get(
@@ -10,19 +14,20 @@ router.get(
   cardsController.getCards,
   (req, res) => {
     //respond to client with cards collection data retrieved from DB
+    console.log('GET REQUEST for cardsController.getCards');
     return res.status(200).json(res.locals.cards);
   }
 );
 
+// CREATE
+router.post('/', cardsController.createCard, (req, res) => {
+  console.log('CREATE REQUEST for cardsController.createCard');
+  return res.status(200).json(res.locals.newCard);
+});
 //DELETE
-router.delete(
-  '/',
-  sessionController.isLoggedIn,
-  cardsController.deleteCard,
-  (req, res) => {
-    //respond to client with cards collection data retrieved from DB
-    return res.status(200).json(res.locals.cards);
-  }
-);
-
+router.delete('/:id', (req, res) => {
+  //respond to client with cards collection data retrieved from DB
+  console.log('DELETE REQUEST for cardsController.deleteCard');
+  return res.status(200).json(res.locals.cards);
+});
 module.exports = router;
