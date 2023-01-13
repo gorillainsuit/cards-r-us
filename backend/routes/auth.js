91e3c32848321a95dfe3f7ea9437f00fdabcf16f
Mathew Hultquist <mathew@gates.services>
2023-01-12 23:08:19 -0500

    Added startSession middleware

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const sessionController = require('../controllers/sessionController');
 
 //POST when user tries to log in
 //hash password before it's saved to database
-router.post('/login', authController.verifyUser, (req, res) => {
-  res.status(200).json(res.locals.user);
-});
+router.post(
+  '/login',
+  authController.verifyUser,
+  sessionController.startSession,
+  (req, res) => {
+    res.status(200).json(res.locals.user);
+  }
+);
 
 // '/signup' Endpoint
-router.post('/signup', authController.signUp, (req, res) => {
-  res.status(200).json(res.locals.newUser);
-});
+router.post(
+  '/signup',
+  authController.signUp,
+  sessionController.startSession,
+  (req, res) => {
+    res.status(200).json(res.locals.newUser);
+  }
+);
 
 module.exports = router;
