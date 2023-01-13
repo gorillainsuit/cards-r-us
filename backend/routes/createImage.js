8b6ab02343d167219d29ba6c14b55dc0e7a8a2a5
JamesJunJieHuang <JamesjunjieHuang@gmail.com>
2023-01-12 22:03:04 -0500

    create image route DALLE API

--- backend/routes/createImage.js
+++ backend/routes/createImage.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const openaiController = require('../controllers/openaiController.js');
 
 router.post('/', openaiController.createImage, (req, res) => {
-    res.status(200).send(res.locals.image);
+    res.status(200).json(res.locals.image);
   }
 );
+
+module.exports = router;
